c3f4bfd3f77f923519c0517bd81d8faf993849a3
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-08-10 23:27:06 +0200

    can isolate col and find its mean

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,29 @@
 import pandas as pd
 from glob import glob
 from os import sep
+import numpy as np
+import matplotlib.pyplot as plt
 
 dir = glob('data/*.csv')
 names = [file[file.index(sep) + 1:file.index('.')] for file in dir]
 files = dict(zip(names, dir))
 
-reader = pd.read_csv(files['Spain'], encoding='utf-8')
-print(reader.head())
+reader = pd.read_csv(files['England'], encoding='utf-8')
+
+# gets all rows and columns containing value
+def get_rows(reader, col:str, value: str):
+    return reader.loc[reader[col] == value]
+
+# gets DataFrame col by its name
+def get_one_col(df, col: str):
+    return df[col]
+
+# calculates mean of DataFrame values of col
+def get_mean_of(col: list or tuple):
+    return round(np.mean(col), 5)
+
+
+if __name__ == '__main__':
+    rows = get_rows(reader, 'HomeTeam', 'Arsenal')
+    col = list(get_one_col(rows, 'FTHG'))
+    print(col)
