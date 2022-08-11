77ca14ad79533118fd4f3c710c2826a23753905b
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-08-11 15:28:12 +0200

    added command prompt args

--- analyzer.py
+++ analyzer.py
@@ -1,43 +1,26 @@
-import pandas as pd
-from glob import glob
-from os import sep
 import numpy as np
 import matplotlib.pyplot as plt
+from reader import read_args, Reader, call_reader
 
-dir = glob('data/*.csv')
-names = [file[file.index(sep) + 1:file.index('.')] for file in dir]
-files = dict(zip(names, dir))
+# # calculates mean of DataFrame values of col
+# def get_mean_of(col: list or tuple):
+#     return round(np.mean(col), 5)
 
-reader = pd.read_csv(files['England'], encoding='utf-8')
+# # draw plot using csv cols as values
+# def draw(title: str, values: list or tuple, xlabel: str = '', ylabel: str = ''):
+#     plt.title(title)
+#     plt.xticks(np.arange(len(col)), np.arange(1, len(col)+1))
+#     plt.yticks(np.arange(0, max(col) + 1))
+#     if xlabel != '':
+#         plt.xlabel(xlabel)
+#     if ylabel != '':
+#         plt.ylabel(ylabel)
+#     plt.plot(values, 'ro', linewidth=2.0)
+#     plt.grid()
+#     plt.show()
 
-# gets all rows and columns containing value
-def get_rows(reader, col:str, value: str):
-    return reader.loc[reader[col] == value]
-
-# gets DataFrame col by its name
-def get_one_col(df, col: str):
-    return df[col]
-
-# calculates mean of DataFrame values of col
-def get_mean_of(col: list or tuple):
-    return round(np.mean(col), 5)
-
-# draw plot using csv cols as values
-def draw(title: str, values: list or tuple, xlabel: str = '', ylabel: str = ''):
-    plt.title(title)
-    plt.xticks(np.arange(len(col)), np.arange(1, len(col)+1))
-    plt.yticks(np.arange(0, max(col) + 1))
-    if xlabel != '':
-        plt.xlabel(xlabel)
-    if ylabel != '':
-        plt.ylabel(ylabel)
-    plt.plot(values, 'r--', linewidth=2.0)
-    plt.grid()
-    plt.show()
-
+# tested with: 
+# python3 -u analyzer.py --file="France" --team="Nantes" --relatedCol="HomeTeam" --cols 'FTHG' (v1)
+# python3 -u analyzer.py -f="France" -t="Nantes" -rc="HomeTeam" -c 'FTHG' (v2)
 if __name__ == '__main__':
-    team = 'Chelsea'
-    rows = get_rows(reader, 'HomeTeam', team)
-    col = list(get_one_col(rows, 'FTHG'))
-    print(col)
-    draw(f'Goals scored at home by {team} during first leg of season 2021-2022', col, 'Matches', 'Goals')
+    call_reader()
---
+++ reader.py
@@ -0,0 +1,67 @@
+from argparse import Namespace
+import argparse
+import pandas as pd
+from glob import glob
+from os import sep
+
+# reads command prompt arguments
+# returns these arguments
+def read_args():
+    parser = argparse.ArgumentParser(description="Arguments and their features",
+                                            usage='%(prog)s [-h] [-f FILE] [-t TEAM] [-rc RELATEDCOL] [-c COLS]',
+                                            epilog="Use -h or --help to see more about arguments")
+    parser.add_argument(
+        '-f', '--file', help='Give filename (extension is optionnal)', type=str, required=True)
+
+    parser.add_argument(
+        '-t', '--team', help='Type a team name', type=str, required=False)
+
+    parser.add_argument(
+        '-rc', '--relatedCol', help='Type column name related to team', type=str, required=False)
+
+    parser.add_argument(
+        '-c', '--cols', help='Type columns name', nargs="+", required=False)
+
+    return parser.parse_args()
+
+class Reader:
+
+    def __init__(self, args: Namespace) -> None:
+        self.args = args
+        self.files = dict()
+        self.reader = None
+
+    def fetch_files(self, dirname: str, extension: str) -> None:
+        dir = glob(f'{dirname}{sep}*.{extension}')
+        names = [file[file.index(sep) + 1:file.index('.')] for file in dir]
+        self.files = dict(zip(names, dir))
+
+    def read(self) -> None:
+        file = self.args.file
+        if file.find('.') != -1:
+            file = file[:file.index(".")]
+        if file not in list(self.files.keys()):
+            return None
+        self.reader = pd.read_csv(self.files[file], encoding='utf-8')
+
+    # gets all data related to value in column
+    def get_rows(self):
+        return self.reader.loc[self.reader[self.args.relatedCol] == self.args.team]
+
+    def get_cols(self, df: pd.DataFrame):
+        return df.loc[:, self.args.cols]
+
+def call_reader():
+    rdr = Reader(read_args())
+    rdr.fetch_files('data', 'csv')
+    rdr.read()
+    if rdr.reader is not None:
+        rows = rdr.get_rows()
+        cols = rdr.get_cols(rows)
+        print(cols)
+
+# tested with: 
+# python3 -u reader.py --file="France" --team="Nantes" --relatedCol="HomeTeam" --cols 'FTHG' (v1)
+# python3 -u reader.py -f="France" -t="Nantes" -rc="HomeTeam" -c 'FTHG' (v2)
+if __name__ == '__main__':
+    call_reader()
